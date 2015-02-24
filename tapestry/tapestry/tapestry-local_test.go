fbd4620282fcc5b45b3c53fa815621c29de62c60
Miguel Martinez Chapa <mm193@cs.brown.edu>
2015-02-24 03:48:36 -0500

    More tests

--- tapestry/tapestry/tapestry-local_test.go
+++ tapestry/tapestry/tapestry-local_test.go
@@ -8,8 +8,8 @@ func CheckFindRoot(node *TapestryNode, target ID, expected ID,
 	t *testing.T) {
 	result, _ := node.findRoot(node.node, target)
 	if !equal_ids(result.Id, expected) {
-		t.Errorf("findRoot of %v is not %v (gives %v)", target, expected,
-			result.Id)
+		t.Errorf("%v: findRoot of %v is not %v (gives %v)", node.node.Id,
+			target, expected, result.Id)
 	}
 }
 
@@ -29,26 +29,39 @@ func TestFindRoot(t *testing.T) {
 	id = ID{7, 0, 0xf, 0xa}
 	node3 := makeTapestryNode(id, mainNode.node.Address, t)
 
-	/*
-		printTable(mainNode.table)
-		printBackpointers(mainNode.backpointers)
-		printTable(node1.table)
-		printBackpointers(node1.backpointers)
-		printTable(node2.table)
-		printBackpointers(node2.backpointers)
-		printTable(node3.table)
-	*/
+	printTable(mainNode.table)
+	printBackpointers(mainNode.backpointers)
+	printTable(node1.table)
+	printBackpointers(node1.backpointers)
+	printTable(node2.table)
+	printBackpointers(node2.backpointers)
+	printTable(node3.table)
 
 	id = ID{3, 0xf, 8, 0xa}
 	CheckFindRoot(mainNode, id, mainNode.node.Id, t)
+	CheckFindRoot(node1, id, mainNode.node.Id, t)
+	CheckFindRoot(node2, id, mainNode.node.Id, t)
+	CheckFindRoot(node3, id, mainNode.node.Id, t)
 	id = ID{5, 2, 0, 0xc}
 	CheckFindRoot(mainNode, id, mainNode.node.Id, t)
+	CheckFindRoot(node1, id, mainNode.node.Id, t)
+	CheckFindRoot(node2, id, mainNode.node.Id, t)
+	CheckFindRoot(node3, id, mainNode.node.Id, t)
 	id = ID{5, 8, 0xf, 0xf}
 	CheckFindRoot(mainNode, id, mainNode.node.Id, t)
+	CheckFindRoot(node1, id, mainNode.node.Id, t)
+	CheckFindRoot(node2, id, mainNode.node.Id, t)
+	CheckFindRoot(node3, id, mainNode.node.Id, t)
 	id = ID{7, 0, 0xc, 3}
 	CheckFindRoot(mainNode, id, node1.node.Id, t)
+	CheckFindRoot(node1, id, node1.node.Id, t)
+	CheckFindRoot(node2, id, node1.node.Id, t)
+	CheckFindRoot(node3, id, node1.node.Id, t)
 	id = ID{6, 0, 0xf, 4}
 	CheckFindRoot(mainNode, id, node2.node.Id, t)
+	CheckFindRoot(node1, id, node2.node.Id, t)
+	CheckFindRoot(node2, id, node2.node.Id, t)
+	CheckFindRoot(node3, id, node2.node.Id, t)
 	id = ID{7, 0, 0xa, 2}
 	CheckFindRoot(mainNode, id, node1.node.Id, t)
 	id = ID{6, 3, 9, 5}
@@ -63,6 +76,10 @@ func TestFindRoot(t *testing.T) {
 	CheckFindRoot(mainNode, id, mainNode.node.Id, t)
 
 	mainNode.Leave()
+
+	id = ID{3, 0xf, 8, 0xa}
+	// CheckFindRoot(mainNode, id, node1.node.Id, t)
+
 	node1.Leave()
 	node2.Leave()
 	node3.Leave()
